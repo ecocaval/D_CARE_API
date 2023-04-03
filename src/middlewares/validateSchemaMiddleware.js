38b368a5fe5a35baf67ab077d71cf9e9ea9a746a
erico <ecocaval@hotmail.com>
2023-04-03 01:41:02 -0300

    refact: improving validateSchemaMiddleware

--- src/middlewares/validateSchemaMiddleware.js
+++ src/middlewares/validateSchemaMiddleware.js
@@ -5,7 +5,7 @@ function validateSchemaMiddleware(schema) {
         const { error } = schema.validate(req.body, { abortEarly: false });
         if (error) {
             const errors = error.details.map((detail) => detail.message);
-            throw err.unprocessableEntityError(errors.join(', '));
+            throw err.unprocessableEntityError(errors.join(', ').replaceAll('\"', ''));
         }
         next();
     };
