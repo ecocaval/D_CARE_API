964a1f42976077b252b91e0e721c3c918502da62
erico <ecocaval@hotmail.com>
2023-04-07 14:54:43 -0300

    feat: adding jest basic setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const config = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleDirectories: ["node_modules", "src"],
+    transform: {
+        ".+\\.ts$": "ts-jest"
+    },
+	testMatch: ["<rootDir>/test/*.(test|spec).ts"]
+};
+
+export default config;
